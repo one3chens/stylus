d5b9e42efaf39390c7b50f6bb8c358931daed9a7
tophf <tophf@gmx.com>
2017-08-21 17:48:30 +0300

    stop current animation first in animateElement()

--- js/dom.js
+++ js/dom.js
@@ -177,7 +177,12 @@ function animateElement(
       }
       resolve();
     });
-    element.classList.add(className);
+    if (element.classList.contains(className)) {
+      element.classList.remove(className);
+      setTimeout(() => element.classList.add(className));
+    } else {
+      element.classList.add(className);
+    }
   });
 }
 
